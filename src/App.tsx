e8c51b39c4d85dbd129a9766827c9418dd0c2171
JustDamion <shirkeydamion@gmail.com>
2025-03-18 18:43:30 -0500

    Added Section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient.tsx";
 import Header from "./components/Header.tsx";
+import Hero from "./components/Hero.tsx";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
