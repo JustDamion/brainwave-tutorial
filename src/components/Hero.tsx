fc6b002e6dd377ceabe50277b2ff3f1b7c888303
JustDamion <shirkeydamion@gmail.com>
2025-03-19 22:52:59 -0500

    Added notification to hero page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,6 +7,7 @@ import Section from "./Section";
 import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants";
 import Generating from "./Generating";
+import Notification from "./Notification";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -65,6 +66,13 @@ const Hero = () => {
                       </li>
                     ))}
                   </ul>
+                </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code generation"
+                  />
                 </ScrollParallax>
               </div>
             </div>
