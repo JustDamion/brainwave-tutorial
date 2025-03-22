bca787a6c8fd1fbd62a15bb387f1e009f5328fb6
JustDamion <shirkeydamion@gmail.com>
2025-03-21 23:35:10 -0500

    Added Collaboration section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient.tsx";
 import Benefits from "./components/Benefits.tsx";
+import Collaboration from "./components/Collaboration.tsx";
 import Header from "./components/Header.tsx";
 import Hero from "./components/Hero.tsx";
 
@@ -10,6 +11,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
